f26455a285a37a2c2fb54dfdfbab99fa60c27c2a
Abel Ciccoli <abelcoli@outlook.fr>
2023-03-05 01:12:01 +0100

    feat(totally my code): added basis of spring boot security

--- dev/src/main/java/fr/cybercicco/dev/config/AuthenticationFilter.java
+++ dev/src/main/java/fr/cybercicco/dev/config/AuthenticationFilter.java
@@ -7,7 +7,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,6 +22,7 @@ import java.io.IOException;
 public class AuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal( @Nonnull HttpServletRequest request, @Nonnull HttpServletResponse response, @Nonnull FilterChain filterChain) throws ServletException, IOException {
@@ -31,7 +36,19 @@ public class AuthenticationFilter extends OncePerRequestFilter {
         token = authHeader.substring(7);
         email = jwtService.extractEmail(token);
         if(email != null && SecurityContextHolder.getContext().getAuthentication() == null){
-
+            UserDetails utilisateur = this.userDetailsService.loadUserByUsername(email);
+            if(jwtService.isTokenValid(token, utilisateur)){
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        utilisateur,
+                        null,
+                        utilisateur.getAuthorities()
+                );
+                authenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
         }
+        filterChain.doFilter(request, response);
     }
 }
--- dev/src/main/java/fr/cybercicco/dev/controller/ConnexionController.java
+++ dev/src/main/java/fr/cybercicco/dev/controller/ConnexionController.java
@@ -1,17 +1,28 @@
 package fr.cybercicco.dev.controller;
 
+import fr.cybercicco.dev.controller.message.AuthenticationRequest;
+import fr.cybercicco.dev.controller.message.AuthenticationResponse;
+import fr.cybercicco.dev.controller.message.RegisterRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("connexion")
 public class ConnexionController {
 
     @GetMapping
-    public ResponseEntity<?> createSession(){
+    public ResponseEntity<?> getSession(){
         return ResponseEntity.ok("you are on the connexion page");
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
+        return null;
+    }
+
+    @PostMapping("authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+        return null;
     }
 
 }
