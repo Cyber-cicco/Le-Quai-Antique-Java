0d21e4079c79774bcf24c4fdb695918e14bd8a03
Abel Ciccoli <abelcoli@outlook.fr>
2023-03-05 19:54:56 +0100

    feat(profile): added a static page for the route profile/connexion

--- front/Quai-antique-front/src/app/pages/connexion/connexion.component.ts
+++ front/Quai-antique-front/src/app/pages/connexion/connexion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -7,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./connexion.component.scss']
 })
 export class ConnexionComponent {
+  formConnexion: FormGroup;
+
+  constructor(private fb:FormBuilder){
+    this.formConnexion = this.fb.group({
+      nom:["", [Validators.required, Validators.minLength(2), Validators.maxLength(255)]],
+      prenom:["",[Validators.required, Validators.minLength(2), Validators.maxLength(255)]],
+      email:["", [Validators.required, Validators.email]],
+      password:["", [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  get nom(){
+    return this.formConnexion.get("nom");
+  }
+
+  get prenom(){
+    return this.formConnexion.get("prenom");
+  }
+
+  get email(){
+    return this.formConnexion.get("email");
+  }
+
+  get password(){
+    return this.formConnexion.get("password")
+  }
 
 }
