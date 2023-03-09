5dc67cc3c2dbd4e9ec97d869fee03486e97ff347
Abel Ciccoli <abelcoli@outlook.fr>
2023-03-09 13:38:25 +0100

    feat(footer): added the footer that takes horaires from the server

---
+++ front/Quai-antique-front/src/app/shared/footer/footer.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import {RestaurantService} from "../../providers/restaurant.service";
+
+@Component({
+  selector: 'qa-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+  message="";
+
+  private jours = ["LUNDI", "MARDI", "MERCREDI", "JEUDI", "VENDREDI", "SAMEDI", "DIMANCHE"]
+  constructor(private restaurantService:RestaurantService) {
+    this.restaurantService.getHorairesCurrentDayAPI(this.jours[new Date().getDay()-1]).subscribe({
+      next: value => {
+        this.message = "Ouvert ce jour entre "
+          + value.fermetureDejeuner +
+          " - " +
+          value.fermetureDejeuner +
+          " et " +
+          value.ouvertureDiner +
+          " - "+
+          value.fermetureDiner;
+      },
+      error: ()=>{
+        this.message = "Fermé ce jour";
+      }
+    })
+  }
+}
