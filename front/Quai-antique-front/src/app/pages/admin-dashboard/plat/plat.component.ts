fba3ffffeb0abed40b1ab0cc0b7b3a8a213333ab
Abel Ciccoli <abelcoli@outlook.fr>
2023-03-17 17:37:14 +0100

    feat(admin plat): patching a plat now fully works

--- front/Quai-antique-front/src/app/pages/admin-dashboard/plat/plat.component.ts
+++ front/Quai-antique-front/src/app/pages/admin-dashboard/plat/plat.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import {Plat} from "../../../models/plat";
 import {PlatService} from "../../../providers/plat.service";
-import {UserService} from "../../../providers/user.service";
 
 @Component({
   selector: 'qa-plat',
@@ -12,7 +11,6 @@ export class PlatComponent {
 
   plats:Plat[] = []
 
-  allergies:string[] = []
 
   constructor(private platService:PlatService) {
     this.platService.getAllPlatsAPI().subscribe(value => {
