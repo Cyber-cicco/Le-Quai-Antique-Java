f8391f90873bbda8484e1902c7c85a8fa4121c96
Abel Ciccoli <abelcoli@outlook.fr>
2023-03-17 22:00:57 +0100

    fix(admin restaurant): created a form allowing you to change horaire. Horaire still can't be modified.

--- front/Quai-antique-front/src/app/pages/admin-dashboard/restaurant/form-horaires/form-horaires.component.ts
+++ front/Quai-antique-front/src/app/pages/admin-dashboard/restaurant/form-horaires/form-horaires.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Horaire} from "../../../../models/horaire";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'qa-form-horaires',
   templateUrl: './form-horaires.component.html',
   styleUrls: ['./form-horaires.component.scss']
 })
-export class FormHorairesComponent {
+export class FormHorairesComponent implements OnChanges{
 
+  @Input() horaires:Horaire[] = [];
+
+  nrSelect = "";
+  formHoraires: FormGroup;
+  horaire: Partial<Horaire> = {};
+  randate= new Date();
+  feedbackMessage = "";
+
+
+  constructor(private fb:FormBuilder) {
+    this.formHoraires = this.fb.group({
+      jour:['',[]],
+      dejeunerDebut: [this.horaire.ouvertureDejeuner,[Validators.required]],
+      dejeunerFin: [this.horaire.ouvertureDejeuner,[]],
+      dinerDebut: [this.horaire.ouvertureDejeuner,[]],
+      dinerFin: [this.horaire.ouvertureDejeuner,[]],
+      ouvertDejeuner:["",[]],
+      ouvertDiner:["",[]]
+    })
+  }
+
+  get dejeunerDebut(){
+    return this.formHoraires.get("dejeunerDebut");
+  }
+
+  get dejeunerFin(){
+    return this.formHoraires.get("dejeunerFin");
+  }
+
+  get dinerDebut(){
+    return this.formHoraires.get("dinerDebut");
+  }
+
+  get dinerFin(){
+    return this.formHoraires.get("dinerFin");
+  }
+
+  get ouvertDejeuner(){
+    return this.formHoraires.get("ouvertDejeuner")
+  }
+
+  get ouvertDiner(){
+    return this.formHoraires.get("ouvertDiner")
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.changeHoraireFromWeekDay("LUNDI");
+  }
+
+  changeHoraireFromWeekDay(day:string){
+    this.nrSelect = day;
+    this.horaire = this.horaires.filter(value => value.jourSemaine == day)[0]
+    this.dejeunerDebut?.setValue(this.horaire?.ouvertureDejeuner);
+    this.dejeunerFin?.setValue(this.horaire?.fermetureDejeuner);
+    this.ouvertDejeuner?.setValue(this.horaire?.ouvertDejeuner);
+    this.dinerDebut?.setValue(this.horaire?.ouvertureDiner);
+    this.ouvertDiner?.setValue(this.horaire?.ouvertDiner);
+    this.dinerFin?.setValue(this.horaire?.fermetureDiner);
+
+  }
+
+  patchHoraire() {
+
+  }
 }
--- front/Quai-antique-front/src/app/pages/admin-dashboard/restaurant/restaurant.component.ts
+++ front/Quai-antique-front/src/app/pages/admin-dashboard/restaurant/restaurant.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Restaurant} from "../../../models/restaurant";
 import {RestaurantService} from "../../../providers/restaurant.service";
+import {Horaire} from "../../../models/horaire";
 
 @Component({
   selector: 'qa-restaurant',
@@ -10,9 +11,14 @@ import {RestaurantService} from "../../../providers/restaurant.service";
 export class RestaurantComponent {
   restaurant: Partial<Restaurant> = {};
 
+  horaires:Horaire[] = []
+
   constructor(private restaurantService:RestaurantService) {
     this.restaurantService.getRestaurant().subscribe(value => {
       this.restaurant = value;
+    })
+    this.restaurantService.getHorairesAPI().subscribe(value => {
+      this.horaires = value;
     })
   }
 
